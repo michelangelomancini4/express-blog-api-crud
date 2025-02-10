f0adbd9aabb7f9728395e7e60d478653e1d53c0c
michelangelomancini <michelangelomancini8@gmail.com>
2025-02-10 14:39:50 +0100

    impostazione primo middleware(controllo se esiste endpoint)

---
+++ middlewares/inexistentEndpoint.js
@@ -0,0 +1,19 @@
+// Esercizio
+// completiamo le nostre API inserendo un middleware per la gestione delle rotte non registrate e uno per la gestione degli errori.
+// Se viene chiamato un endpoint inesistente, un middleware dovrà rispondere un messaggio e uno status appropriato.
+
+// Facciamo il controllo
+function inexistentEndpoint (req, res, next) {
+    //Imposto lo status 404
+    res.status(404).json({
+        error: "Non Trovato",
+        message: "Endpoint inesistente"
+        })
+   
+    }
+
+
+// Esporto il middleware
+module.exports = inexistentEndpoint;
+
+// Se viene generato un errore, un middleware si occuperà di rispondere con un messaggio e uno status appropriato.
